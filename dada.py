f4c775a9d904245fa2bab1aef11f2318b9d306a4
vaca22 <wsmslgh@gmail.com>
2022-07-17 14:17:20 +0800

    xcf

--- dada.py
+++ dada.py
@@ -9,7 +9,7 @@
 
 
 from PyQt5 import QtCore, QtGui, QtWidgets
-
+from PyQt5.QtWidgets import QMessageBox
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -41,7 +41,7 @@ class Ui_MainWindow(object):
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
-        self.dog.clicked.connect(lambda :self.show_dog())
+        self.dog.clicked.connect(lambda :self.show_pop())
         self.cat.clicked.connect(lambda :self.show_cat())
 
     def show_dog(self):
@@ -54,3 +54,19 @@ class Ui_MainWindow(object):
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
         self.cat.setText(_translate("MainWindow", "gaga"))
         self.dog.setText(_translate("MainWindow", "dada"))
+
+    def show_pop(self):
+        msg = QMessageBox()
+        msg.setWindowTitle("Tutorial on PyQt5")
+        msg.setText("This is the main text")
+        msg.setIcon(QMessageBox.Critical)
+        msg.setStandardButtons(QMessageBox.Cancel|QMessageBox.Retry|QMessageBox.Ignore)
+        msg.setDefaultButton(QMessageBox.Retry)
+        msg.setInformativeText("gaga")
+        msg.setDetailedText("details")
+        msg.buttonClicked.connect(self.popup_button)
+        x = msg.exec_()
+
+    def popup_button(self,i):
+        print(i.text())
+
